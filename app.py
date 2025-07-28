2ee3b0755633e2e81fb0204207e2cdc5b8eccc32
dotpycoder <akulpra12@gmail.com>
2025-07-28 15:40:22 +0530

    Keys checking

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from rag_chatbotv2 import generate_answer
-
+from rag_chatbotv2 import generate_answer, OPENAI_API_KEY, PINECONE_API_KEY, INDEX_NAME
 app = FastAPI()
 
 @app.get("/")
@@ -11,3 +11,11 @@ def root():
 def ask(query: str):
     answer = generate_answer(query)
     return {"query": query, "answer": answer}
+
+@app.get("/check_keys")
+def check_keys():
+    return {
+        "OPENAI_API_KEY_set": OPENAI_API_KEY is not None,
+        "PINECONE_API_KEY_set": PINECONE_API_KEY is not None,
+        "PINECONE_INDEX_NAME": INDEX_NAME
+    }
